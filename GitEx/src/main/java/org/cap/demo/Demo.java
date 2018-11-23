5550643f9711d48ca9089f7ab1fb7be0e6bb7fc6
igupta <igupta@DIN73003017.corp.capgemini.com>
2018-11-23 14:21:43 +0530

    my first git

---
+++ GitEx/src/main/java/org/cap/demo/Demo.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("hii");
+		sayHello();
+
+	}
+	
+	public static void sayHello() {
+		System.out.println("hELLO");
+	}
+
+}
